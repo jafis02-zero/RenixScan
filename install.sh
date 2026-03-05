27d69ed5a7a49850f4a3f9108b1049568b8265e7
Jafis KA <jafis0001@gmail.com>
2026-03-04 23:50:21 -0500

    Dependency Installation Script

--- install.sh
+++ install.sh
@@ -6,30 +6,32 @@ echo "        Installing RenixScan"
 echo "===================================="
 echo ""
 
+# Update system
 echo "[1] Updating system..."
-sudo apt update
+sudo apt update -y
 
+# Install dependencies
 echo "[2] Installing dependencies..."
-sudo apt install -y golang
-sudo apt install -y assetfinder
-sudo apt install -y python3-pip
+sudo apt install -y golang assetfinder python3 python3-pip curl
 
+# Install subfinder
 echo "[3] Installing subfinder..."
 go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
 
+# Move subfinder to PATH
+sudo cp ~/go/bin/subfinder /usr/local/bin/
+
+# Install Python modules
 echo "[4] Installing Python modules..."
-pip3 install requests
+pip3 install requests colorama tabulate --break-system-packages
 
+# Install RenixScan globally
 echo "[5] Installing RenixScan..."
-
-sudo cp renixscan.py /usr/bin/renixscan
-sudo chmod +x /usr/bin/renixscan
+sudo cp renixscan.py /usr/local/bin/renixscan
+sudo chmod +x /usr/local/bin/renixscan
 
 echo ""
 echo "===================================="
 echo " RenixScan Installed Successfully!"
 echo "===================================="
 echo ""
-echo "Run the tool using:"
-echo "renixscan"
-Logo
